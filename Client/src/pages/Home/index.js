f1026133727931f05e9cff3f50c5708f96b7b25f
nhtt01 <nhtantai01@gmail.com>
2023-08-11 21:43:56 +0700

    commi

--- Client/src/pages/Home/index.js
+++ Client/src/pages/Home/index.js
@@ -4,7 +4,10 @@ import Slider from "../../components/Slider";
 import TypeProducts from "../../components/TypeProducts";
 import CardProduct from "../../components/Card";
 import Navbar from "../../components/Navbar";
-
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ButtonComponent from "../../components/Button";
 const Home = () => {
   const arr = ["TV", "Laptop", "Điện thoại", "Tai nghe", "Loa"];
   return (
@@ -14,10 +17,44 @@ const Home = () => {
           return <TypeProducts name={item} key={item} />;
         })}
       </TypeProductst>
-       <Slider/>
-       <CardProduct/>
-       <Navbar/>
-      <div>Home</div>
+      <Slider />
+      <Row>
+        <Col md={2}>
+          <CardProduct />
+        </Col>
+        <Col md={2}>
+          <CardProduct />
+        </Col>
+        <Col md={2}>
+          <CardProduct />
+        </Col>
+        <Col md={2}>
+          <CardProduct />
+        </Col>
+        <Col md={2}>
+          <CardProduct />
+        </Col>
+        <Col md={2}>
+          <CardProduct />
+        </Col>
+        <Col md={2}>
+          <CardProduct />
+        </Col>
+        <Col md={2}>
+          <CardProduct />
+        </Col>
+      </Row>
+      <ButtonComponent
+        textbutton="Xem Thêm"
+        type="outline"
+        styleButton={{
+          border: "1px solid rgb(11,116,229)",
+          color: " rgb(11,116,229)",
+          width: "240px",
+          height: "38px",
+          borderRadius: "4px",
+        }}
+      />
     </div>
   );
 };
