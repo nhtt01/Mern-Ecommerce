f5d317b385f1f610c601cf5ba86bc29bd1c7a51e
nhtt01 <nhtantai01@gmail.com>
2023-08-25 16:41:26 +0700

    COMMIT

--- Client/src/components/ProductDetail/index.js
+++ Client/src/components/ProductDetail/index.js
@@ -1,66 +1,18 @@
+import { Image } from 'antd';
 import React, { useRef, useState } from 'react';
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/free-mode';
-import 'swiper/css/navigation';
-import 'swiper/css/thumbs';
-
-
-
-// import required modules
-import { FreeMode, Navigation, Thumbs } from 'swiper/modules';
-
+import imageProduct  from '../../assets/images/Products/Iphone14Detail.jpg'
 export default function ProductDetailComponent() {
-  const [thumbsSwiper, setThumbsSwiper] = useState(null);
 
   return (
-    <>
-      <Swiper
-       
-        spaceBetween={10}
-        navigation={true}
-        thumbs={{ swiper: thumbsSwiper }}
-        modules={[FreeMode, Navigation, Thumbs]}
-        className="mySwiper2"
-      >
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
-        </SwiperSlide>      
-      </Swiper>
-      <Swiper
-        onSwiper={setThumbsSwiper}
-        spaceBetween={10}
-        slidesPerView={4}
-        freeMode={true}
-        watchSlidesProgress={true}
-        modules={[FreeMode, Navigation, Thumbs]}
-        className="mySwiper"
-      >
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
-        </SwiperSlide>    
-      </Swiper>
-    </>
+    <div className='container'>
+      <div className='row'>
+        <div className='col-4'>
+          <Image src={imageProduct} alt='Product Detail' preview/>
+        </div>
+        <div className='col-8'></div>
+      </div>
+    </div>
   );
 }
+
+
